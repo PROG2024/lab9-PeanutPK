81875f4c2edc97359fb00ac90b08d7c808aacc36
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-03-12 15:52:15 +0700

    make three cases in the test_circle.py

--- test_circle.py
+++ test_circle.py
@@ -7,6 +7,32 @@ Write unit tests as described in README.md.
 3. Unit test that circle constructor raises exception of radius is negative.
 
 """
+import math
+import unittest
+
 from circle import Circle
 
-# TODO write 3 tests as described above
+
+class Test(unittest.TestCase):
+    """Test add area function in circle and invalid radius input"""
+
+    def test_add_area(self):
+        """Test normal area adding for Circle object with radius 2 and 4"""
+        # math.hypot(x, y) returns sqrt(x*x + y*y)
+        rad = math.hypot(2, 4)
+        self.assertTrue(rad == Circle(2).add_area(Circle(4)).radius)
+
+    def test_add_area_zero(self):
+        """Try edge case with one of them has zero as radius"""
+        rad = math.hypot(0, 4)
+        self.assertTrue(rad == Circle(0).add_area(Circle(4)).radius)
+
+    def test_negative_radius(self):
+        """Invalid negative value input for Circle object"""
+        with self.assertRaises(ValueError):
+            Circle(-1)  # try input an invalid negative number of -1
+
+
+if __name__ == '__main__':
+    start = Test()
+    start.run()
