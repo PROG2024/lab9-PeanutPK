430716263a476d232202ec13bfc27e9b3a10105c
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-03-13 12:04:03 +0700

    make a Singleton counter and test_counter.py

--- counter/counter.py
+++ counter/counter.py
@@ -8,15 +8,47 @@
    1. in Counter, do not use any static methods except __new__.
       You may not have a __new__ depending on how you implement the singleton.
 """
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(levelname)s %(funcName)8s: %(message)s")
+
 
 class Counter:
+    """Counter class that counts and add when increment is called"""
+    __instance = None
 
     def __init__(self):
         self.__count = 0
 
-
     def __str__(self):
         return f"{self.__count}"
 
-    #TODO write count property
-    #TODO write increment method
+    def __new__(cls, *args, **kwargs):
+        """Create the same instance when """
+        if cls.__instance is None:
+            logging.info("Create new Counter")
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    @property
+    def count(self):
+        """Return count value"""
+        return self.__count
+
+    def increment(self):
+        """Add one to count and return the new value"""
+        self.__count += 1
+        return self.__count
+
+
+if __name__ == '__main__':
+    # should show info when create
+    c = Counter()
+    print(f"count value is zero: {c.count}")
+    print(f"increment one time: {c.increment()}")
+    print(f"count value is one: {c.count}")
+    print(f"id of count object 1: {id(c)}")
+    # should not show info when create and stay the same
+    c2 = Counter()
+    print(f"id of count object 2: {id(c2)}")
--- counter/test_counter.py
+++ counter/test_counter.py
@@ -7,3 +7,22 @@
 
    You can use pytest or unittest.
 """
+import unittest
+from counter import Counter
+
+
+class SingleTonTest(unittest.TestCase):
+    def setUp(self):
+        self.c1 = Counter()
+        self.c2 = Counter()
+
+    def test_same_id(self):
+        self.assertEqual(id(self.c1), id(self.c2))
+
+    def test_same_value(self):
+        self.assertEqual(self.c1.count, self.c2.count)
+        self.c1.increment()
+        self.assertEqual(self.c1.count, self.c2.count)
+
+    def test_same_object(self):
+        self.assertEqual(self.c1, self.c2)
